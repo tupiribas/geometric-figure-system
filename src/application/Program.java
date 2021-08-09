80e6b195f3f1756976a51f08d639036eebe471f2
Tupi Guedes <tupiguedes@outlook.com>
2021-08-09 02:26:29 -0300

    Implementing to wildcard type list

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Circle;
 import entities.Rectangle;
 import entities.Shape;
@@ -8,12 +12,30 @@ import entities.enums.Color;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Shape re = new Rectangle(Color.valueOf("RED"), 2.4307631204964398648, 2.4307631204964398648);
-		System.out.println(re.area());
-		
-		Shape ci = new Circle(Color.valueOf("BLUE"), 3.0);	
-		System.out.println(ci.area());
+
+		Locale.setDefault(Locale.US);
+
+		List<Shape> myShape = new ArrayList<>();
+		myShape.add(new Rectangle(Color.BLUE, 2.43, 3.54));
+		myShape.add(new Circle(Color.BLACK, 5.1));
+
+		List<Circle> myCircle = new ArrayList<>();
+		myCircle.add(new Circle(Color.BLACK, 8.1));
+
+		System.out.println("Total area: " + String.format("%.2f", totalArea(myCircle, myShape)));
+
+	}
+
+	private static Double totalArea(List<? extends Shape> myShape, List<Shape> my) {
+		double sum = 0.0;
+		double sum2 = 0.0;
+		for (Shape list : myShape) {
+			sum += list.area();
+		}
+		for (Shape o : my) {
+			sum2 += o.area();
+		}
+		return sum + sum2;
 	}
 
 }
